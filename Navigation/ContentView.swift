2797257870b3a87fae3c140c19b685f462c98d1e
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-07 10:15:32 -0400

    added a second view and a home return to home button

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    
         NavigationStack {
-            Text("This is the root view 🌳")
-            NavigationLink(destination: Text("You have arrvied to the second view 🎉 ")) {
-                Text("Click me!")//the lable content shows to the user the link
-            }//part of navigation link
-        }//closing navigation stack
+            VStack{
+                Text("This is the root view 🌳")
+                NavigationLink(destination: Second_View()) {
+                    Text("Click me!")//the lable content shows to the user the link
+                }
+                    NavigationLink(destination: Text("No number before 1,000 has the letter A in it")) {
+                        Text("Click me for a fun fact!")
+                    }//part of navigation link 2
+                }//part of navigation link 1
+            .navigationTitle("Home🏡")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+            }//closing navigation stack
+        
     }//closing body
 }//closing struct
 
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  Second View.swift
+//  Navigation
+//
+//  Created by Scholar on 8/7/25.
+//
+
+import SwiftUI
+
+struct Second_View: View {
+    var body: some View {
+        Text("You have arrvied to the second view 🎉")
+    }
+}
+
+#Preview {
+    Second_View()
+}
